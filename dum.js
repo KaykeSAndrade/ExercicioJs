2d8821bc08ac158466e942558cde4c01c10b14ea
Kayke SAndrade <kaykewwe1212@hotmail.com>
2022-04-25 11:54:17 -0300

    first commit

---
+++ dum.js
@@ -0,0 +1,12 @@
+function clicou(){
+
+    document.querySelector("#quadrado").innerHTML = "clicou"
+}
+
+function tirouMouse(){
+    document.querySelector("#quadrado").innerHTML = "Tirou o Mouse"
+}
+
+function passouoMouse(){
+    document.querySelector("#quadrado").innerHTML = "Passou o Mouse"
+}
